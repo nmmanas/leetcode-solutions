af02954cf7563f74679cba8055f0820fc763e844
Manas <nmmanas@gmail.com>
2024-07-27 14:20:55 +0530

    l23: step1&2 problem statement and test cases

--- L23_merge_k_sorted_lists.py
+++ L23_merge_k_sorted_lists.py
@@ -2,8 +2,9 @@
 Leetcode link: https://leetcode.com/problems/merge-k-sorted-lists
 """
 
-from typing import List
+from typing import List, Optional
 
+from datastructures import ListNode
 from test import evaluate_test_cases
 
 
@@ -18,17 +19,26 @@ class Solution:
     6.Fix complexities and efficiencies, repeat 3-6
 
     Question:
-    <question from leetcode>
+    You are given an array of `k` linked-lists `lists`, each linked-list
+    is sorted in ascending order. Merge all the linked-lists into 
+    one sorted linked-list and return it.
 
     Constraints:
-    <constraints from leetcode>
+        `k == lists.length`
+        `0 <= k <= 10^4`
+        `0 <= lists[i].length <= 500`
+        `-104 <= lists[i][j] <= 10^4`
+        `lists[i]` is sorted in ascending order.
+        The sum of `lists[i].length` will not exceed `10^4`.
 
     Problem statement (Step 1):
-    <problem in own words>
+    Given a list of linked-lists, which are sorted in ascending order, merge
+    the linked lists in to one sorted linked list and return it
 
     Input/Output formats (Step 1):
+    ** provided by leetcode **
     """
-    def solution(self, input1: List[int], input2: int) -> int:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         """
         Pseudo Code (Step 3):
 
@@ -40,15 +50,76 @@ class Solution:
 def load_test_cases():
     """
     List of identified test cases covering standard, edge cases (Step 2):
-    1. first edge case
+    1. multiple lists
+    2. all empty lists
+    3. one empty list
+    4. all single value
+    5. one single value
+    6. singe and empty lists
+    7. leetcode example 1
+    8. leetcode example 2
+    9. leetcode example 3
     """
     test_cases = []
-    # 1. 1 pile, hours less than pile size
+
+    # 1. multiple lists
     test_cases.append({
         'input': {
-            'piles': [30,11,23,4,20],
-            'h': 6
-        }, 'output': 23
+            'lists': ListNode.parse_list_of_lists([[1,3,5,7,9],[0,2,4,6,8,10]]),
+        }, 'output': ListNode.parse_list([0,1,2,3,4,5,6,7,8,9,10])
+    })
+    test_cases.append({
+        'input': {
+            'lists': ListNode.parse_list_of_lists([[1,3,5,7,9],[1,3,5,7,9]]),
+        }, 'output': ListNode.parse_list([1,1,3,3,5,5,7,7,9,9])
+    })
+    # 2. all empty lists
+    test_cases.append({
+        'input': {
+            'lists': ListNode.parse_list_of_lists([[],[],[]]),
+        }, 'output': ListNode.parse_list([])
+    })
+    # 3. one empty list
+    test_cases.append({
+        'input': {
+            'lists': ListNode.parse_list_of_lists([[1,3,5,7,9],[]]),
+        }, 'output': ListNode.parse_list([1,3,5,7,9])
+    })
+    # 4. all single value
+    test_cases.append({
+        'input': {
+            'lists': ListNode.parse_list_of_lists([[1],[3],[5],[7],[9]]),
+        }, 'output': ListNode.parse_list([1,3,5,7,9])
+    })
+    # 5. one single value
+    test_cases.append({
+        'input': {
+            'lists': ListNode.parse_list_of_lists([[1,3,7,9],[5]]),
+        }, 'output': ListNode.parse_list([1,3,5,7,9])
+    })
+    # 6. singe and empty lists
+    test_cases.append({
+        'input': {
+            'lists': ListNode.parse_list_of_lists([[1,3,7,9],[5],[]]),
+        }, 'output': ListNode.parse_list([1,3,5,7,9])
+    })
+    # 7. leetcode example 1
+    test_cases.append({
+        'input': {
+            'lists': ListNode.parse_list_of_lists([[1,4,5],[1,3,4],[2,6]]),
+        }, 'output': ListNode.parse_list([1,1,2,3,4,4,5,6])
+    })
+    # 8. leetcode example 2
+    test_cases.append({
+        'input': {
+            'lists': ListNode.parse_list_of_lists([]),
+        }, 'output': ListNode.parse_list([])
+    })
+    # 9. leetcode example 3
+    test_cases.append({
+        'input': {
+            'lists': ListNode.parse_list_of_lists([[]]),
+        }, 'output': ListNode.parse_list([])
     })
 
     return test_cases
@@ -56,4 +127,4 @@ def load_test_cases():
 if __name__ == "__main__":
     test_cases = load_test_cases()
     solution = Solution()
-    evaluate_test_cases(solution.solution, test_cases)
+    evaluate_test_cases(solution.mergeKLists, test_cases)
